ca79e4c2dff7b65c71df13145a600be35180c00d
kyran <kyranperdue@gmail.com>
2020-01-15 14:13:03 -0800

    created course class mad student class public

---
+++ ConsoleApp1/Course.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NestedObjects
+{
+    public class Course
+    {
+        public string CourseTitle { get; set; }
+
+        public string Description { get; set; }
+
+        public int CourseID { get; set; }
+
+        public byte Credits { get; set; }
+    }
+}

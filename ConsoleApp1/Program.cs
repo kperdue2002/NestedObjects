82328d4f7bbb1036a65231ceb82398e8f6b9aa5e
kyran <kyranperdue@gmail.com>
2020-01-15 14:57:46 -0800

    did some test coade

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -10,6 +10,21 @@ namespace NestedObjects
     {
         static void Main(string[] args)
         {
+            Student s = new Student();
+
+            Course cpw212 = new Course()
+            {
+                CourseID = 1,
+                CourseTitle = "Advanced >NET",
+                Credits = 5,
+                Description = "Advanced C#"
+            };
+            s.Schedule.Add(cpw212);
+
+            List<Course> courses = s.Schedule;
+            courses.Add(cpw212);
+            courses.Add(cpw212);
+
         }
     }
 }
